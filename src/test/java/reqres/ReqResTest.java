3ad213b11ac42fbf86d8207312c24ac767c3c7fe
Mario Aliaga <mario@aliaga.dev>
2021-04-27 16:21:12 -0400

    Initial Commit

---
+++ src/test/java/reqres/ReqResTest.java
@@ -0,0 +1,29 @@
+package reqres;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.Matchers.notNullValue;
+
+public class ReqResTest {
+
+    @Test
+    public void loginOk() {
+        RestAssured
+                .given()
+                .log().all()
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "    \"email\": \"eve.holt@reqres.in\",\n" +
+                        "    \"password\": \"cityslicka\"\n" +
+                        "}")
+                .post("https://reqres.in/api/login")
+                .then()
+                .log()
+                .all()
+                .statusCode(200)
+                .body("token", notNullValue());
+
+    }
+}

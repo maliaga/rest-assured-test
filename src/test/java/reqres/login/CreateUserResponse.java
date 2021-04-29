7bc3f5fa238ba413b06174773d47c67e94b72a09
Mario Aliaga <mario@aliaga.dev>
2021-04-28 21:12:33 -0400

    Add serialized request and response

---
+++ src/test/java/reqres/login/CreateUserResponse.java
@@ -0,0 +1,50 @@
+package reqres.login;
+
+public class CreateUserResponse{
+	private String createdAt;
+	private String name;
+	private String id;
+	private String job;
+
+	public void setCreatedAt(String createdAt){
+		this.createdAt = createdAt;
+	}
+
+	public String getCreatedAt(){
+		return createdAt;
+	}
+
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public void setId(String id){
+		this.id = id;
+	}
+
+	public String getId(){
+		return id;
+	}
+
+	public void setJob(String job){
+		this.job = job;
+	}
+
+	public String getJob(){
+		return job;
+	}
+
+	@Override
+	public String toString() {
+		return "CreateUserResponse{" +
+				"createdAt='" + createdAt + '\'' +
+				", name='" + name + '\'' +
+				", id='" + id + '\'' +
+				", job='" + job + '\'' +
+				'}';
+	}
+}
